999a999cb92d3011cf8726a17c0ac162002c7a5e
selbowgreaser <selbowgreaser@gmail.com>
2022-09-04 09:45:16 +0300

    Initial Commit

---
+++ src/YetAnotherHelloWorld.java
@@ -0,0 +1,31 @@
+/**
+ * <h1>Знакомство со средой разработки</h1>
+ *
+ * <h2>Удобные сокращения</h2>
+ * psvm - public static void main(String[] args) {} <br>
+ * sout - System.out.println(); <br><br>
+ *
+ * <h2>HotKeys</h2>
+ * Shift + F6 - переименовать везде <br>
+ * Ctrl + Alt + Shift + L - автоформатирование кода <br>
+ * Ctrl + Space - автопродолжение <br><br>
+ *
+ * <h2>Запуск программы</h2>
+ * Правой кнопкой по вкладке класса -> Run (Ctrl + Shift + F10 in first time or Shift + F10 in another time) <br><br>
+ *
+ * <h2>Изменение передаваемых аргументов командной строки</h2>
+ * Левой кнопкой по конфигурации, затем Edit Configurations... и вводим аргументы в поле Program Arguments <br>
+ *
+ * @author selbowgreaser
+ * @version 1.0
+ */
+public class YetAnotherHelloWorld {
+    /**
+     * Program entry point.
+     *
+     * @param cmdLineArgs command-line arguments
+     */
+    public static void main(String[] cmdLineArgs) {
+        System.out.println("Hello World! ");
+    }
+}
