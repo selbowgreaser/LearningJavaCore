c75ecb4442aa33a254216c49ab7f8c769936a514
aafrolov <TgkKm7xG>
2022-09-06 09:20:08 +0300

    Первая неделя занятий
    Пройден раздел Массивы и строки

---
+++ BasicSyntax/ArraysAndStrings/src/ZerosArray.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ZerosArray {
+    public static void main(String[] args) {
+        int[] zerosArray = new int[0];
+        System.out.println(Arrays.toString(zerosArray));
+    }
+}
