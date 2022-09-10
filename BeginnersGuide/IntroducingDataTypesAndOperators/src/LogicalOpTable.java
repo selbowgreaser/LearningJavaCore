d65311e2b165af9f878ea76dcefeb4c487fccebe
selbowgreaser <selbowgreaser@gmail.com>
2022-09-10 17:35:45 +0300

    Знакомство с типами данных и операторами
    Исправлены замечания

--- BeginnersGuide/IntroducingDataTypesAndOperators/src/LogicalOpTable.java
+++ BeginnersGuide/IntroducingDataTypesAndOperators/src/LogicalOpTable.java
@@ -1,19 +1,20 @@
 public class LogicalOpTable {
     public static void main(String[] args) {
-        boolean a, b;
+        boolean a;
+        boolean b;
 
-        System.out.printf("a\tb\tAND\tOR\tXOR\tNOT\t\n");
+        System.out.print("a\tb\tAND\tOR\tXOR\tNOT\n");
         a = false;
         b = false;
-        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\t\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
+        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
         a = false;
         b = true;
-        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\t\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
+        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
         a = true;
         b = false;
-        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\t\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
+        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
         a = true;
         b = true;
-        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\t\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
+        System.out.printf("%d\t%d\t%d\t%d\t%d\t%d\n", a ? 1 : 0, b ? 1 : 0, a && b ? 1 : 0, a || b ? 1 : 0, a ^ b ? 1 : 0, !a ? 1 : 0);
     }
 }
